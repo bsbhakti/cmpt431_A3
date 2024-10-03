76a382be3a00bb3109437bcc8ee2db1ec8c72938
bhakti <bsb10@sfu.ca>
2024-10-03 14:53:32 -0700

    Combined output produced

--- pull.sh
+++ pull.sh
@@ -5,4 +5,4 @@
 #SBATCH --mem=5G
 #SBATCH --partition=slow
 
-srun python /home/bsb10/Downloads/scripts/page_rank_tester.pyc --execPath=/home/bsb10/Downloads/ --scriptPath=/home/bsb10/Downloads/scripts/page_rank_evaluator.pyc --inputPath=/home/bsb10/Downloads/input_graphs/
+srun python /home/bsb10/Downloads/cmpt431_A3/scripts/page_rank_tester.pyc --execPath=/home/bsb10/Downloads/cmpt431_A3/ --scriptPath=/home/bsb10/Downloads/cmpt431_A3/scripts/page_rank_evaluator.pyc --inputPath=/home/bsb10/Downloads/cmpt431_A3/input_graphs/
