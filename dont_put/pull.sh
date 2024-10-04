b514bc09766d0aec804ec34dd90f99e728911f14
bhakti <bsb10@sfu.ca>
2024-10-03 17:16:26 -0700

    Made tar

--- dont_put/pull.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-#SBATCH --cpus-per-task=8
-#SBATCH --time=10:00
-#SBATCH --mem=5G
-#SBATCH --partition=slow
-
-srun python /home/bsb10/Downloads/cmpt431_A3/scripts/page_rank_tester.pyc --execPath=/home/bsb10/Downloads/cmpt431_A3/ --scriptPath=/home/bsb10/Downloads/cmpt431_A3/scripts/page_rank_evaluator.pyc --inputPath=/home/bsb10/Downloads/cmpt431_A3/input_graphs/
